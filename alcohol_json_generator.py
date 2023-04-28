06fb4255270adaa9cec54c9f02e04d1481ca74b5
Mateusz Machnowski <ma.machnowsk@student.uw.edu.pl>
2023-04-28 16:20:10 +0200

    final touches

---
+++ alcohol_json_generator.py
@@ -0,0 +1,104 @@
+from typing import Dict, List, Union
+import json
+import pandas as pd
+from collections import defaultdict
+
+
+def get_alcohol_category(alcohol_type: str) -> str:
+    # Determine the category of the given alcohol_type
+    if alcohol_type.lower() in [
+        "brandy",
+        "cognac",
+        "gin",
+        "mezcal",
+        "rum",
+        "tequila",
+        "vodka",
+        "whisky",
+    ]:
+        return "distilled"
+    elif alcohol_type.lower() in [
+        "beer", 
+        "cider", 
+        "sake", 
+        "wine"
+    ]:
+        return "fermented"
+    else:
+        return ""
+
+
+def create_json_dict(excel_file: str, keywords_file: str) -> List[Dict]:
+    # Load keywords from the provided JSON file
+    with open(keywords_file) as f:
+        keywords_dict = json.load(f)
+
+    # Load alcohol brands and types from the provided Excel file
+    df = pd.read_excel(excel_file)
+
+    # Initialize alcohol_dict with defaultdict for simplified dictionary creation
+    alcohol_dict = {
+        "distilled": defaultdict(lambda: {"name": "", "keywords": [], "brands": []}),
+        "fermented": defaultdict(lambda: {"name": "", "keywords": [], "brands": []}),
+    }
+
+    # Iterate through each row in the DataFrame and populate alcohol_dict
+    for _, row in df.iterrows():
+        brand = row["Brand"]
+        alcohol_type = row["Type"]
+
+        # Check if both alcohol type and brand are present
+        if pd.isna(alcohol_type) or pd.isna(brand):
+            print(f"Skipping row due to missing alcohol type or brand: {row}")
+            continue
+
+        # Get the alcohol category (distilled or fermented) or continue if unknown
+        category = get_alcohol_category(alcohol_type)
+        if not category:
+            print(f"Unknown alcohol type: {alcohol_type}, for brand {brand}")
+            continue
+
+        # Get or create the type_dict for the specific alcohol type in the category
+        type_dict = alcohol_dict[category][alcohol_type.lower()]
+        type_dict["name"] = alcohol_type.lower()
+        type_dict["keywords"] = keywords_dict[category].get(alcohol_type.lower(), [])
+        type_dict["brands"].append(brand)
+
+    # Create the final alcohol_list by converting the defaultdict values to lists
+    alcohol_list = [
+        {
+            "name": "distilled",
+            "keywords": keywords_dict["distilled"].get("general", []),
+            "children": list(alcohol_dict["distilled"].values()),
+        },
+        {
+            "name": "fermented",
+            "keywords": keywords_dict["fermented"].get("general", []),
+            "children": list(alcohol_dict["fermented"].values()),
+        },
+    ]
+
+    return alcohol_list
+
+
+def save_json(json_dict: List[Dict], file_path: str) -> None:
+    # Save the provided JSON dictionary to a file with the given file_path
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(json_dict, f, ensure_ascii=False, indent=4)
+
+
+def main() -> None:
+    # Define input and output file paths
+    keywords_file = 'keywords.json'
+    excel_file = 'alcohols.xlsx'
+
+    # Create the JSON dictionary using the provided input files
+    json_dict = create_json_dict(excel_file, keywords_file)
+
+    # Save the JSON dictionary to a file
+    json_file = 'alcohols.json'
+    save_json(json_dict, json_file)
+
+
+if __name__ == '__main__':
+    main()
