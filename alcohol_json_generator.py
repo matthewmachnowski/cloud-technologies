b25ffdc6dec9c3d78d5294b91918895755ab68da
Mateusz Machnowski <ma.machnowsk@student.uw.edu.pl>
2023-05-04 15:59:16 +0200

    final touches

--- alcohol_json_generator.py
+++ alcohol_json_generator.py
@@ -1,40 +1,46 @@
-from typing import Dict, List, Union
+from typing import Dict, List
 import json
 import pandas as pd
 from collections import defaultdict
 
 
+DISTILLED_ALCOHOLS = [
+    "brandy",
+    "cognac",
+    "gin",
+    "mezcal",
+    "rum",
+    "tequila",
+    "vodka",
+    "whisky",
+    'liqueur'
+]
+
+FERMENTED_ALCOHOLS = [
+    "beer",
+    "cider",
+    "sake",
+    "wine",
+]
+
 def get_alcohol_category(alcohol_type: str) -> str:
-    # Determine the category of the given alcohol_type
-    if alcohol_type.lower() in [
-        "brandy",
-        "cognac",
-        "gin",
-        "mezcal",
-        "rum",
-        "tequila",
-        "vodka",
-        "whisky",
-    ]:
+    """Determine the category of the given alcohol_type."""
+    if alcohol_type.lower() in DISTILLED_ALCOHOLS:
         return "distilled"
-    elif alcohol_type.lower() in [
-        "beer", 
-        "cider", 
-        "sake", 
-        "wine"
-    ]:
+    elif alcohol_type.lower() in FERMENTED_ALCOHOLS:
         return "fermented"
     else:
         return ""
 
 
-def create_json_dict(excel_file: str, keywords_file: str) -> List[Dict]:
+def create_json_dict(csv_file: str, keywords_file: str) -> List[Dict]:
+    """Create a JSON dictionary from the given CSV file and keywords JSON file."""
     # Load keywords from the provided JSON file
     with open(keywords_file) as f:
         keywords_dict = json.load(f)
 
-    # Load alcohol brands and types from the provided Excel file
-    df = pd.read_excel(excel_file)
+    # Load alcohol brands and types from the provided CSV file
+    df = pd.read_csv(csv_file)
 
     # Initialize alcohol_dict with defaultdict for simplified dictionary creation
     alcohol_dict = {
@@ -67,33 +73,30 @@ def create_json_dict(excel_file: str, keywords_file: str) -> List[Dict]:
     # Create the final alcohol_list by converting the defaultdict values to lists
     alcohol_list = [
         {
-            "name": "distilled",
-            "keywords": keywords_dict["distilled"].get("general", []),
-            "children": list(alcohol_dict["distilled"].values()),
-        },
-        {
-            "name": "fermented",
-            "keywords": keywords_dict["fermented"].get("general", []),
-            "children": list(alcohol_dict["fermented"].values()),
-        },
+            "name": category,
+            "keywords": keywords_dict[category].get("general", []),
+            "children": list(alcohol_dict[category].values()),
+        }
+        for category in ["distilled", "fermented"]
     ]
 
     return alcohol_list
 
 
 def save_json(json_dict: List[Dict], file_path: str) -> None:
-    # Save the provided JSON dictionary to a file with the given file_path
+    """Save the provided JSON dictionary to a file with the given file_path."""
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(json_dict, f, ensure_ascii=False, indent=4)
 
 
 def main() -> None:
+    """Main function for the script."""
     # Define input and output file paths
     keywords_file = 'keywords.json'
-    excel_file = 'alcohols.xlsx'
+    csv_file = 'alcohols.csv'
 
     # Create the JSON dictionary using the provided input files
-    json_dict = create_json_dict(excel_file, keywords_file)
+    json_dict = create_json_dict(csv_file, keywords_file)
 
     # Save the JSON dictionary to a file
     json_file = 'alcohols.json'
